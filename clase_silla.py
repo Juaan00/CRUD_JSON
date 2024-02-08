66a2ac388c4feffe8b002804b8dd43910e376d3b
Juaan00 <145508368+Juaan00@users.noreply.github.com>
2024-02-08 20:37:58 +0000

    yes

---
+++ clase_silla.py
@@ -0,0 +1,12 @@
+class Silla:
+    color ="Cafe"
+    altura = "1.25"
+    peso = 1
+    
+    def reclinar ():
+        return True
+    
+silla_Odontologo = Silla
+
+print(silla_Odontologo.reclinar())
+print(silla_Odontologo.color)
