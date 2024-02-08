15d9ad8b6836004a119679070a7b2ea5d52a24f1
Juaan00 <145508368+Juaan00@users.noreply.github.com>
2024-02-08 20:56:57 +0000

    yes

--- clase_silla.py
+++ clase_silla.py
@@ -3,10 +3,17 @@ class Silla:
     altura = "1.25"
     peso = 1
     
-    def reclinar ():
+    def __init__(self, color, material, patas):
+        self.color = color
+        self.material = material
+        self.patas = patas
+    
+    def reclinar (self):
         return True
     
-silla_Odontologo = Silla
+silla_Odontologo = Silla('Naranja','Aluminio',4)
+#silla_Odontologo.color = 'Verde'
 
 print(silla_Odontologo.reclinar())
 print(silla_Odontologo.color)
+print(silla_Odontologo.patas)
